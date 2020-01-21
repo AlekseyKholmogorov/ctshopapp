2b9c29ed3dc588c4a5cf8bcc5dd86a0d6a59a6d0
adhol <adhol1989@gmail.com>
2020-01-21 18:26:39 +0400

    removed SpringBootServletInitializer

--- src/main/java/com/ardecs/ctshop/Application.java
+++ src/main/java/com/ardecs/ctshop/Application.java
@@ -2,12 +2,12 @@ package com.ardecs.ctshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class Application extends SpringBootServletInitializer {
+//extends SpringBootServletInitializer нужен для запуска приложений Spring Boot из классического архива WAR, но не мне.
+public class Application  {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
